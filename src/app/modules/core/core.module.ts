3e33d0fa812487d79f13e27461c55afbd7685b1a
Oleksandr Ryzhyk <uebersetzer94@gmail.com>
2018-11-24 12:13:37 +0200

    moved core module into modules directive

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DynamicFormModule } from '../dynamic-form/dynamic-form.module';
+import { MatButtonModule, MatIconModule } from '@angular/material';
+import { DndCarouselComponent } from './components/dnd-carousel/dnd-carousel.component';
+import { RangeDirective } from './directives/range.directive';
+import { LetDirective } from './directives/let.directive';
+import { CarouselDirective } from './directives/carousel.directive';
 
 @NgModule({
     imports: [
         CommonModule,
-        FormsModule,
-        ReactiveFormsModule,
-        DynamicFormModule
+        MatButtonModule,
+        MatIconModule
     ],
     declarations: [
+        DndCarouselComponent,
+
+        RangeDirective,
+        LetDirective,
+        CarouselDirective,
     ],
     exports: [
+        MatButtonModule,
+        MatIconModule,
+
+        DndCarouselComponent,
+
+        RangeDirective,
+        LetDirective,
+        CarouselDirective
     ]
 })
 export class CoreModule {
