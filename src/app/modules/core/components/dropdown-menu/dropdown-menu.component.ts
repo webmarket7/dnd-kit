378dce94f5aac8f04c4bdbe9792faa2c9f7a3222
Oleksandr Ryzhyk <uebersetzer94@gmail.com>
2018-12-08 17:54:45 +0200

    DEF-3 - improved dropdown-menu component

--- src/app/modules/core/components/dropdown-menu/dropdown-menu.component.ts
+++ src/app/modules/core/components/dropdown-menu/dropdown-menu.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component, EventEmitter, InjectionToken, OnDestroy, OnInit, Output, TemplateRef,
+    ViewChild, ViewEncapsulation, ChangeDetectionStrategy
+} from '@angular/core';
+
+const DROPDOWN_MENU_PANEL = new InjectionToken<any>('DROPDOWN_MENU_PANEL');
 
 @Component({
     selector: 'app-dropdown-menu',
     templateUrl: './dropdown-menu.component.pug',
     styleUrls: ['./dropdown-menu.component.scss'],
-    exportAs: 'dropdownMenuComponent'
+    exportAs: 'dropdownMenuComponent',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    encapsulation: ViewEncapsulation.None,
+    providers: [
+        {provide: DROPDOWN_MENU_PANEL, useExisting: DropdownMenuComponent}
+    ]
 })
-export class DropdownMenuComponent implements OnInit {
+export class DropdownMenuComponent implements OnInit, OnDestroy {
+
+    @Output() readonly closed: EventEmitter<void | 'click' | 'keydown' | 'tab'> =
+        new EventEmitter<void | 'click' | 'keydown' | 'tab'>();
+
+    @ViewChild(TemplateRef) templateRef: TemplateRef<any>;
 
     constructor() {
     }
@@ -14,4 +29,7 @@ export class DropdownMenuComponent implements OnInit {
     ngOnInit() {
     }
 
+    ngOnDestroy() {
+        this.closed.complete();
+    }
 }
--- src/app/modules/core/directives/dropdown-menu.directive.ts
+++ src/app/modules/core/directives/dropdown-menu.directive.ts
@@ -1,47 +1,160 @@
-import { AfterViewInit, Directive, ElementRef, HostListener, Input, ViewContainerRef } from '@angular/core';
-import { PortalOutlet, TemplatePortal } from '@angular/cdk/portal';
-import { ConnectedPosition, Overlay, OverlayConfig } from '@angular/cdk/overlay';
+import { AfterViewInit, Directive, ElementRef, HostListener, Input, OnDestroy, ViewContainerRef } from '@angular/core';
+import { ComponentPortal, Portal, PortalOutlet, TemplatePortal } from '@angular/cdk/portal';
+import {
+    ConnectedPosition, FlexibleConnectedPositionStrategy, Overlay, OverlayConfig,
+    OverlayRef, PositionStrategy
+} from '@angular/cdk/overlay';
+import { merge, Observable, Subscription } from 'rxjs/index';
 
 @Directive({
     selector: '[appDropdownMenu]'
 })
-export class DropdownMenuDirective implements AfterViewInit {
+export class DropdownMenuDirective implements AfterViewInit, OnDestroy {
 
-    @Input('appDropdownMenu') componentRef: any;
+    private _portal: TemplatePortal;
+    private _overlayRef: OverlayRef | null = null;
+    private _menuOpen = false;
+    private _closeSubscription = Subscription.EMPTY;
+    private _menuCloseSubscription = Subscription.EMPTY;
+    private _menu: any;
 
-    overlayRef: PortalOutlet;
-    dropdownMenuPortal: TemplatePortal<any>;
+    @Input('appDropdownMenu')
+    get menu() {
+        return this._menu;
+    }
+
+    set menu(menu: any) {
+        if (menu === this._menu) {
+            return;
+        }
+
+        this._menu = menu;
+        this._menuCloseSubscription.unsubscribe();
+
+        console.log('Menu', menu);
+
+        if (menu) {
+            this._menuCloseSubscription = menu.closed
+                .asObservable()
+                .subscribe(reason => {
+                    this._destroyMenu();
+                });
+        }
+    }
 
     @HostListener('click', ['$event.target'])
     onClick(btn: HTMLBaseElement) {
-        console.log('Component Ref', this.componentRef);
+        this.toggleMenu();
+    }
 
+    constructor(private elRef: ElementRef,
+                private _viewContainerRef: ViewContainerRef,
+                private _overlay: Overlay) {
+    }
+
+    ngAfterViewInit(): void {
+    }
+
+    ngOnDestroy(): void {
+        this._cleanUpSubscriptions();
+    }
+
+    private _getPortal(): TemplatePortal {
+        if (!this._portal || this._portal.templateRef !== this.menu.templateRef) {
+            this._portal = new TemplatePortal(this.menu.templateRef, this._viewContainerRef);
+            console.log(this._portal);
+        }
+
+        return this._portal;
+    }
+
+    private _getPositionStrategy(): PositionStrategy {
         const defaultPosition = {
-            originX: 'start',
+            originX: 'end',
             originY: 'bottom',
-            overlayX: 'start',
+            overlayX: 'end',
             overlayY: 'top'
         } as ConnectedPosition;
 
-        const positionStrategy = this.overlay.position()
+        return this._overlay.position()
             .flexibleConnectedTo(this.elRef)
             .withPositions([defaultPosition]);
-
-        const overlayConfig = new OverlayConfig({
-            positionStrategy
-        });
-
-        this.overlayRef = this.overlay.create(overlayConfig);
-        this.dropdownMenuPortal = new TemplatePortal(this.componentRef, this.viewContainerRef);
-        const componentRef = this.overlayRef.attach(this.dropdownMenuPortal);
     }
 
-    constructor(
-        private elRef: ElementRef,
-        private viewContainerRef: ViewContainerRef,
-        private overlay: Overlay
-    ) {}
+    private _getOverlayConfig(): OverlayConfig {
+        return new OverlayConfig({
+            positionStrategy: this._getPositionStrategy()
+        });
+    }
 
-    ngAfterViewInit(): void {
+    private _subscribeToPositions(position: FlexibleConnectedPositionStrategy): void {
+        // if (this.menu.setPositionClasses) {
+        //     position.positionChanges.subscribe(change => {
+        //         const posX: MenuPositionX = change.connectionPair.overlayX === 'start' ? 'after' : 'before';
+        //         const posY: MenuPositionY = change.connectionPair.overlayY === 'top' ? 'below' : 'above';
+        //
+        //         this.menu.setPositionClasses!(posX, posY);
+        //     });
+        // }
+    }
+
+    private _createOverlay(): OverlayRef {
+        if (!this._overlayRef) {
+            const config = this._getOverlayConfig();
+            this._subscribeToPositions(config.positionStrategy as FlexibleConnectedPositionStrategy);
+            this._overlayRef = this._overlay.create(config);
+
+            this._overlayRef.keydownEvents().subscribe();
+        }
+
+        return this._overlayRef;
+    }
+
+    private _menuClosingActions(): Observable<any> {
+        if (this._overlayRef) {
+            const backdrop = this._overlayRef.backdropClick();
+            const detachments = this._overlayRef.detachments();
+
+            return merge(backdrop, detachments);
+        }
+    }
+
+    private _cleanUpSubscriptions(): void {
+        this._closeSubscription.unsubscribe();
+        this._menuCloseSubscription.unsubscribe();
+    }
+
+    toggleMenu(): void {
+        return this._menuOpen ? this.closeMenu() : this.openMenu();
+    }
+
+    openMenu(): void {
+        if (this._menuOpen) {
+            return;
+        }
+
+        const overlayRef = this._createOverlay();
+
+        overlayRef.attach(this._getPortal());
+
+        this._closeSubscription = this._menuClosingActions()
+            .subscribe(() => this.closeMenu());
+
+        this._menuOpen = true;
+    }
+
+    closeMenu(): void {
+        this.menu.closed.emit();
+    }
+
+    private _destroyMenu() {
+        if (!this._overlayRef || !this._menuOpen) {
+            return;
+        }
+
+        this._closeSubscription.unsubscribe();
+        this._menuCloseSubscription.unsubscribe();
+        this._overlayRef.detach();
+        this._menuOpen = false;
     }
 }
